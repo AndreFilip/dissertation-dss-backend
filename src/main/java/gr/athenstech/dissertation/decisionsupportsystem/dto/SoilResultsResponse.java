2234920864bb00a11fa3fbd70913b6b6a2f2ee59
andreas <afilippatos85@gmail.com>
2019-06-01 20:45:38 +0300

    SoilResultsResponse implements Serializable

--- src/main/java/gr/athenstech/dissertation/decisionsupportsystem/dto/SoilResultsResponse.java
+++ src/main/java/gr/athenstech/dissertation/decisionsupportsystem/dto/SoilResultsResponse.java
@@ -1,8 +1,14 @@
 package gr.athenstech.dissertation.decisionsupportsystem.dto;
 
+import java.io.Serializable;
 import java.util.List;
 
-public class SoilResultsResponse {
+public class SoilResultsResponse implements Serializable{
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
 	private String carbonResult;
 	private String phResult;
 	private String cationResult;
